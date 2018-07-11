0aedc77caadf97adeb8eeeea6e9fa6f6a05973d3
Kenny <kennybrown11@hotmail.com>
2018-07-11 02:18:35 +0000

    Project tiny app - shortening url

---
+++ express_server.js
@@ -0,0 +1,54 @@
+var express = require("express");
+var app = express();
+var PORT = 8080;
+const bodyParser = require("body-parser");
+app.use(bodyParser.urlencoded({extended: true}));
+
+
+let urlDatabase = {
+  "b2xVn2": "http://www.lighthouselabs.ca",
+  "9sm5xK": "http://www.google.com"
+};
+
+function generateRandomString() {
+let text = "";
+var possible = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
+
+  for (var i = 0; i < 6; i++)
+    text += possible.charAt(Math.floor(Math.random() * possible.length));
+
+  return text;
+}
+
+
+app.set('view engine', 'ejs');
+
+app.get("/urls", (req, res) => {
+  res.render("urls_index", { urls: urlDatabase });
+});
+
+app.get("/urls/new", (req, res) => {
+  res.render("urls_new");
+});
+
+app.post("/urls", (req, res) => {
+  console.log(req.body);  
+  res.send("Ok");
+});
+
+app.get("/urls/:id", (req, res) => {
+    let shortURL =  req.params.id;
+    res.render("urls_show", { shortURL: shortURL, urlDatabase: urlDatabase } );
+  });
+
+app.get("/hello", (req, res) => {
+  res.end("<html><body>Hello <b>World</b></body></html>\n");
+});
+
+app.get("/", (req, res) => {
+  res.end("hello");
+});
+
+app.listen(PORT, () =>{
+  console.log(`Example app listening on port ${PORT}!`);
+});
