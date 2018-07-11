b21457401a2f833282bc5041f5dbb5a6a9a9c9d3
Kenny <kennybrown11@hotmail.com>
2018-07-11 23:25:42 +0000

    updating urls

--- express_server.js
+++ express_server.js
@@ -4,7 +4,7 @@ var PORT = 8080;
 const bodyParser = require("body-parser");
 app.use(bodyParser.urlencoded({extended: true}));
 
-// stores key and value of shortURL and longURL
+// urlDatabase 
 let urlDatabase = {
   "b2xVn2": "http://www.lighthouselabs.ca",
   "9sm5xK": "http://www.google.com"
@@ -29,7 +29,7 @@ app.get("/urls", (req, res) => {
   res.render("urls_index", { urls: urlDatabase });
 });
 
-// 
+// let user enter new url
 app.get("/urls/new", (req, res) => {
   res.render("urls_new");
 });
@@ -41,29 +41,33 @@ app.post("/urls", (req, res) => {
   res.redirect(`/urls/${shortURL}`);
 });
 
-app.get("/u/:shortURL", (req, res) => {
-  let longURL = req.params.longURL;
-  res.redirect(longURL);
-});
 
+
+//update longURL
 app.get("/urls/:id", (req, res) => {
     let shortURL = req.params.id;
     res.render("urls_show", { shortURL: shortURL, urlDatabase: urlDatabase } );
   });
 
+app.get("/urls/:id/update ", (req, res) => {
+    let longURL = req.body.longURL;
+    let shortURL = req.params.id;
+    urlDatabase[shortURL] = longURL;
+    res.render("urls_show", { shortURL: shortURL, urlDatabase: urlDatabase });
+});  
+
+app.post("/urls/:id/update ", (req, res) => {
+    let longURL = req.body.longURL;
+    urlDatabase[shortURL] = longURL;
+    res.redirect("urls_index");
+});
+
 // deletes url from database
 app.post("/urls/:id/delete", (req, res) => {
     delete urlDatabase[req.params.id]
-    res.redirect(`/urls/`);
+    res.render("urls_index",{ urls: urlDatabase });
   });  
 
-app.get("/hello", (req, res) => {
-  res.end("<html><body>Hello <b>World</b></body></html>\n");
-});
-
-app.get("/", (req, res) => {
-  res.end("hello");
-});
 
 app.listen(PORT, () =>{
   console.log(`Example app listening on port ${PORT}!`);
