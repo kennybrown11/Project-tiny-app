579d581ee2acd793bb0c490666d411bc6c020594
Kenny <kennybrown11@hotmail.com>
2018-07-12 20:00:44 +0000

    everything before this commit is terrible

--- express_server.js
+++ express_server.js
@@ -1,29 +1,32 @@
-var express = require("express");
-var cookieParser = require('cookie-parser')
-var app = express();
+const express = require("express");
+const cookieParser = require('cookie-parser')
+const app = express();
 app.use(cookieParser())
-var PORT = 8080;
+const PORT = 8080;
 const bodyParser = require("body-parser");
 app.use(bodyParser.urlencoded({extended: true}));
 app.set('view engine', 'ejs');
+app.use(bodyParser.json({}));
 
 // urlDatabase 
-let urlDatabase = {
+const urlDatabase = {
   "b2xVn2": "http://www.lighthouselabs.ca",
   "9sm5xK": "http://www.google.com"
 };
 
 // generates a random six digit alphanumerical string represent shortURL
 function generateRandomString() {
-let text = "";
+let shortURL = "";
 var possible = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
 
   for (var i = 0; i < 6; i++)
-    text += possible.charAt(Math.floor(Math.random() * possible.length));
+    shortURL += possible.charAt(Math.floor(Math.random() * possible.length));
 
-  return text;
+  return shortURL;
 }
 
+
+//login saves cookie
 app.post("/login", (req, res) => {
   res.cookie('username', req.body.username)
   res.redirect("/urls");
@@ -36,17 +39,28 @@ app.get("/urls", (req, res) => {
   res.render("urls_index", templateVars);
 });
 
+  // If url is valid, redirect to longURL else 404
+app.get('/u/:shortURL', (req, res) => { 
+  let longURL = 
+ urlDatabase[req.params.shortURL];
+    res.redirect(longURL);
+});
+
 // let user enter new url
 app.get("/urls/new", (req, res) => {
   let templateVars = { urls: urlDatabase, username: req.cookies["username"] }
   res.render("urls_new", templateVars);
 });
 
+// generates new short url
 app.post("/urls", (req, res) => {
-  let longURL = req.body.longURL;
   let shortURL = generateRandomString();
-  urlDatabase[shortURL] = longURL
-  res.redirect(`/urls/${shortURL}`);
+  let object = {
+    shortURL,
+    longURL: req.body.longURL };
+  urlDatabase[shortURL] = object;
+
+  res.status(201).json(object);
 });
 
 // logout
@@ -55,8 +69,6 @@ app.post("/logout", (req, res) => {
   res.redirect("/urls")
 });
 
-
-
 //update longURL
 app.get("/urls/:id", (req, res) => {
     let shortURL = req.params.id;
@@ -64,6 +76,7 @@ app.get("/urls/:id", (req, res) => {
     res.render("urls_show", templateVars);
   });
 
+// provides form for updating url
 app.get("/urls/:id/update ", (req, res) => {
     let longURL = req.body.longURL;
     let shortURL = req.params.id;
@@ -72,16 +85,18 @@ app.get("/urls/:id/update ", (req, res) => {
     res.render("urls_show", templateVars);
 });  
 
-app.post("/urls/:id/update ", (req, res) => {
+// posts updated url
+app.post("/urls/:id", (req, res) => {
     let longURL = req.body.longURL;
+    shortURL = req.params.id;
     urlDatabase[shortURL] = longURL;
-    res.redirect("urls_index");
+    res.redirect("/urls");
 });
 
 // deletes url from database
 app.post("/urls/:id/delete", (req, res) => {
     delete urlDatabase[req.params.id]
-    res.render("urls_index",{ urls: urlDatabase });
+    res.redirect("/urls");
   });  
 
 
