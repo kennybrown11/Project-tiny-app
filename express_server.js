94ea170f182ad330ec53537bcef59bd777e843b2
Kenny <kennybrown11@hotmail.com>
2018-07-12 21:55:33 +0000

    task 4 - creates a registration handler

--- express_server.js
+++ express_server.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const cookieParser = require('cookie-parser')
+//const bcrypt = require('bcrypt');
 const app = express();
 app.use(cookieParser())
 const PORT = 8080;
@@ -7,12 +8,43 @@ const bodyParser = require("body-parser");
 app.use(bodyParser.urlencoded({extended: true}));
 app.set('view engine', 'ejs');
 app.use(bodyParser.json({}));
+const cookieSession = require ('cookie-session'); 
+app.use(cookieSession({
+  name: 'session',
+  keys: [process.env.SESSION_SECRET || 'secret-string'],
+}));
 
-// urlDatabase 
-const urlDatabase = {
-  "b2xVn2": "http://www.lighthouselabs.ca",
-  "9sm5xK": "http://www.google.com"
+const users = { 
+  "userRandomID": {
+    id: "userRandomID", 
+    email: "user@example.com", 
+    password: "purple-monkey-dinosaur"
+  },
+ "user2RandomID": {
+    id: "user2RandomID", 
+    email: "user2@example.com", 
+    password: "dishwasher-funk"
+  }
 };
+
+app.get('/register', (req, res) => {
+  let templateVars = { urls: urlDatabase, username: req.cookies["randomID"] }
+  res.render('email_register', templateVars);
+});
+
+app.post('/register', (req, res) => {
+  res.cookie('randomID', req.body.email)
+  const randomID = generateRandomString();
+  users[randomID] = {
+    id: randomID,
+    email: req.body.email,
+    password: req.body.password,
+  }
+  req.session.user_id = randomID;
+  console.log(req.body.email);
+  res.redirect('/urls');
+});
+
 
 // generates a random six digit alphanumerical string represent shortURL
 function generateRandomString() {
@@ -24,7 +56,11 @@ var possible = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
 
   return shortURL;
 }
-
+// urlDatabase 
+const urlDatabase = {
+  "b2xVn2": "http://www.lighthouselabs.ca",
+  "9sm5xK": "http://www.google.com"
+};
 
 //login saves cookie
 app.post("/login", (req, res) => {
