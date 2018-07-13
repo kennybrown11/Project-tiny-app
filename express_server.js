37f5db0cafd515a22dcf65622ab9dd0602646e71
Kenny <kennybrown11@hotmail.com>
2018-07-13 01:41:57 +0000

    create login page

--- express_server.js
+++ express_server.js
@@ -14,6 +14,12 @@ app.use(cookieSession({
   keys: [process.env.SESSION_SECRET || 'secret-string'],
 }));
 
+// urlDatabase 
+const urlDatabase = {
+  "b2xVn2": "http://www.lighthouselabs.ca",
+  "9sm5xK": "http://www.google.com"
+};
+
 const users = { 
   "userRandomID": {
     id: "userRandomID", 
@@ -27,41 +33,54 @@ const users = {
   }
 };
 
+// generates a random six digit alphanumerical string represent shortURL
+function generateRandomString() {
+  let shortURL = "";
+  var possible = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
+  
+    for (var i = 0; i < 6; i++)
+      shortURL += possible.charAt(Math.floor(Math.random() * possible.length));
+  
+    return shortURL;
+  }
+
 app.get('/register', (req, res) => {
-  let templateVars = { urls: urlDatabase, username: req.cookies["randomID"] }
+  let templateVars = { urls: urlDatabase, username: req.cookies[req.session.user_id] }
   res.render('email_register', templateVars);
 });
 
 app.post('/register', (req, res) => {
   res.cookie('randomID', req.body.email)
-  const randomID = generateRandomString();
-  users[randomID] = {
-    id: randomID,
-    email: req.body.email,
-    password: req.body.password,
-  }
-  req.session.user_id = randomID;
-  console.log(req.body.email);
-  res.redirect('/urls');
+ 
+    if (!req.body.password || !req.body.email) {
+      res.status(400).send("Enter an email and password");
+      return;
+    } 
+    let arr = [];
+    for (let id in users) {
+      arr.push(users[id].email)};
+
+    if  (arr.includes(req.body.email)) {
+      res.status(400).send("That email already exists");
+      return;
+    }
+    
+    else {
+      const randomID = generateRandomString();
+      req.session.user_id = randomID;
+      users[randomID] = {
+        id: randomID,
+        email: req.body.email,
+        password: req.body.password
+       };
+    req.session.user_id = randomID; 
+    res.redirect("/urls");
+   }
+});  
+
+app.get("/login", (req, res) =>{
+  res.render("login")
 });
-
-
-// generates a random six digit alphanumerical string represent shortURL
-function generateRandomString() {
-let shortURL = "";
-var possible = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
-
-  for (var i = 0; i < 6; i++)
-    shortURL += possible.charAt(Math.floor(Math.random() * possible.length));
-
-  return shortURL;
-}
-// urlDatabase 
-const urlDatabase = {
-  "b2xVn2": "http://www.lighthouselabs.ca",
-  "9sm5xK": "http://www.google.com"
-};
-
 //login saves cookie
 app.post("/login", (req, res) => {
   res.cookie('username', req.body.username)
@@ -71,7 +90,7 @@ app.post("/login", (req, res) => {
 
 //  index of stored urls
 app.get("/urls", (req, res) => {  
-  let templateVars = { urls: urlDatabase, username: req.cookies["username"] }
+  let templateVars = { urls: urlDatabase, username: req.cookies[req.session.user_id] }
   res.render("urls_index", templateVars);
 });
 
@@ -84,7 +103,7 @@ app.get('/u/:shortURL', (req, res) => {
 
 // let user enter new url
 app.get("/urls/new", (req, res) => {
-  let templateVars = { urls: urlDatabase, username: req.cookies["username"] }
+  let templateVars = { urls: urlDatabase, username: req.cookies[req.session.user_id] }
   res.render("urls_new", templateVars);
 });
 
@@ -101,14 +120,14 @@ app.post("/urls", (req, res) => {
 
 // logout
 app.post("/logout", (req, res) => {
-  res.clearCookie('username')
+  res.clearCookie(req.session.user_id)
   res.redirect("/urls")
 });
 
 //update longURL
 app.get("/urls/:id", (req, res) => {
     let shortURL = req.params.id;
-    let templateVars = {shortURL: shortURL, urls: urlDatabase, username: req.cookies["username"] }
+    let templateVars = {shortURL: shortURL, urls: urlDatabase, username: req.cookies[req.session.user_id] }
     res.render("urls_show", templateVars);
   });
 
@@ -116,7 +135,7 @@ app.get("/urls/:id", (req, res) => {
 app.get("/urls/:id/update ", (req, res) => {
     let longURL = req.body.longURL;
     let shortURL = req.params.id;
-    let templateVars = {shortURL: shortURL, urls: urlDatabase, username: req.cookies["username"] }
+    let templateVars = {shortURL: shortURL, urls: urlDatabase, username: req.cookies[req.session.user_id] }
     urlDatabase[shortURL] = longURL;
     res.render("urls_show", templateVars);
 });  
