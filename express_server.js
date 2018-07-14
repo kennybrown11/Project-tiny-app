0f8ae999f14d3d768bdf55b50bf9b8097bb9098e
Kenny <kennybrown11@hotmail.com>
2018-07-14 04:47:03 +0000

    fixed registration

--- express_server.js
+++ express_server.js
@@ -6,6 +6,13 @@ const cookieSession = require ('cookie-session');
 app.set('view engine', 'ejs');
 app.use(bodyParser.json({}));
 
+//=====CookieSession=====
+app.use(cookieSession({
+  name: 'session',
+  keys: [process.env.SESSION_SECRET || 'secret-string'],
+}));
+
+
 //=====BodyParser=====
 app.use(bodyParser.urlencoded({extended: true}));
 
@@ -18,13 +25,6 @@ app.use((req, res, next) => {
   res.locals.user = users[req.session.user_id];
   next();
 });
-
-//=====CookieSession=====
-app.use(cookieSession({
-  name: 'session',
-  keys: [process.env.SESSION_SECRET || 'secret-string'],
-}));
-
 
 //=====Databases======
 
@@ -59,94 +59,123 @@ function generateRandomString() {
     return generatedID;
   }
 
+   
+  function checkEmail(email) {
+    for (var id in users) {
+      if (users[id].email === email)
+      return true;
+    }
+    return false;
+  };
+
 //===== Get Street=====
 
-//=====email register===== 
-app.get('/register', (req, res) => {
- 
-  let templateVars = { urls: urlDatabase, username: req.cookies[req.session.user_id] }
-  res.render('email_register', templateVars);
-  
+//=====Renders Login Page=====
+app.get("/login", (req, res) => {
+  let userId = req.session.user_id;
+  if(userId){
+    res.redirect('/urls');
+  } else {
+    res.render('login');
+  }
 });
 
-//=====update longURL=====
-app.get("/urls/:id", (req, res) => {
-  let shortURL = req.params.id;
-  let templateVars = {shortURL: shortURL, urls: urlDatabase, username: req.cookies[req.session.user_id] }
-  res.render("urls_show", templateVars);
+//=====Email Register===== 
+app.get("/register", (req, res) => {
+  let userId = req.session.user_id;
+  if(userId){
+    res.redirect('/urls');
+  } else {
+    res.render("email_register");
+  }
 });
-
-//=====provides form for updating url=====
-app.get("/urls/:id/update ", (req, res) => {
-  let longURL = req.body.longURL;
-  let shortURL = req.params.id;
-  let templateVars = {shortURL: shortURL, urls: urlDatabase, username: req.cookies[req.session.user_id] }
-  urlDatabase[shortURL] = longURL;
-  res.render("urls_show", templateVars);
-});  
-
 
 //=====index of stored urls=====
-app.get("/urls", (req, res) => {  
-  let templateVars = { urls: urlDatabase, username: req.cookies[req.session.user_id] }
-  res.render("urls_index", templateVars);
-});
-
-//=====If url is valid, redirect to longURL else 404=====
-app.get('/u/:shortURL', (req, res) => { 
-  let longURL = 
- urlDatabase[req.params.shortURL];
-    res.redirect(longURL);
+app.get("/urls", (req, res) => {
+  let userId = req.session.user_id;
+  let user = users[userId];
+  if(userId) {
+    let templateVars = {
+      urls: urlDatabase,
+      user: user
+    };
+    res.render("urls_index", templateVars);
+  } else {
+    res.status(400).send('Please login or register to view your urls.');
+  }
 });
 
 //=====let user enter new url=====
 app.get("/urls/new", (req, res) => {
-  if (res.locals.user_id) {
-  let templateVars = { urls: urlDatabase, username: req.cookies[req.session.user_id] }
+  let userId = req.session.user_id;
+  let user = users[userId];
+  let templateVars = { urls: urlDatabase, user: user};
   res.render("urls_new", templateVars);
+});
+
+//=====provides form for updating url=====
+app.get("/urls/:id/ ", (req, res) => {
+  let longURL = req.body.longURL;
+  let shortURL = req.params.id;
+  let templateVars = {urlObj: urlDatabase[req.params.id], user: user};
+  urlDatabase[shortURL] = longURL;
+  res.render("urls_show", templateVars);
+
+  if (urlDatabase[req.params.id]) {
+  res.redirect('/urls');
+} else {
+  res.status(400);
+}
+});  
+//======== ShortURL Page =========
+app.get("/u/:id", (req, res) => {
+  if (!urlDatabase[req.params.id]) {
+    res.status(400).send('URL submitted is incorrect.');
+  } else {
+    res.redirect(urlDatabase[req.params.id].longURL);
   }
 });
 
-//=====checks if email is exisiting or present=====
-app.post('/register', (req, res) => {
-  res.cookie('randomID', req.body.email)
- 
-    if (!req.body.password || !req.body.email) {
-      res.status(400).send("Enter an email and password");
-      return;
-    } 
 
-//=====loops though users{}, if email is already in {}, nope =====
-    let arr = [];
-    for (let id in users) {
-      arr.push(users[id].email)};
-
-    if  (arr.includes(req.body.email)) {
-      res.status(400).send("That email already exists");
-      return;
-    }
-    
-//===== adds new user to users object=====
-    else {
-      const randomID = generateRandomString();
-      req.session.user_id = randomID;
-      users[randomID] = {
-        id: randomID,
-        email: req.body.email,
-        password: req.body.password
-       };
-    req.session.user_id = randomID; 
-    res.redirect("/urls");
-   }
-});  
-
-//=====renders login page=====
-app.get("/login", (req, res) =>{
-  res.render("login")
+app.get("/urls.json", (req, res) => {
+  res.json(urlDatabase);
 });
 
+//===== Redirect to the login=====
+app.get("/", (req, res) => {
+  let userId = req.session.user_id;
+  if(userId){
+    res.redirect('/urls');
+  } else {
+    res.redirect('/login');
+  }
+});
 
 //=====Post Street=====
+
+
+//=====checks if email is exisiting or present=====
+app.post("/register", (req, res) => {
+var userID = generateRandomString();
+let newUser = {
+  id: userID,
+  email: req.body.email,
+  password: req.body.password }
+
+  if (!newUser.password || !newUser.email) {
+    res.status(400).send("Enter an email and password");
+  } 
+
+//=====loops though users{}, if email is already in {}, nope =====
+  else if (checkEmail(newUser.email)) {
+    res.status(400).send('A user with this email already exists.');
+  } else {
+    users[userID] = newUser;
+    req.session.user_id = userID;
+    res.redirect('/urls');
+  }
+});
+    
 
 //=====login saves cookie=====
 app.post("/login", (req, res) => {
